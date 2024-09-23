d6664f71e4f8c5dc684c27d784e5abfa952e5890
vraj_patel <vrajhp0254@gmail.com>
2024-09-23 22:33:13 +0530

    create interaction

--- index.js
+++ index.js
@@ -8,4 +8,16 @@ client.on('createmessage'(message =>{
   })
 }))
 
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) return;
+
+  const { commandName } = interaction;
+
+  if (commandName === 'ping') {
+    await interaction.reply('Pong!');
+  } else if (commandName === 'beep') {
+    await interaction.reply('Boop!');
+  }
+});
+
 client.login(process.env.TOKEN)
