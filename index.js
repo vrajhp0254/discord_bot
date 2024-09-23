ef75ce905dc57a72ee9eb52ea0677a6bca4283c4
vraj_patel <vrajhp0254@gmail.com>
2024-09-23 22:24:33 +0530

    setup discord.js

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-const express = require("express");
+import { Client, GatewayIntentBits } from 'discord.js';
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages,GatewayIntentBits.MessageContent] });
 
-const app = express();
+client.on('createmessage'(message =>{
+  if(user.author.bot) return;
+  message.reply({
+    content: `Hello ${message.author.username}!`
+  })
+}))
 
-app.get("/", (req, res) => {
-  res.send("a discord_bot");
-});
-
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
-
-module.exports = app;
+client.login(process.env.TOKEN)
